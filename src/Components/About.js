ead0a0ec9a0379ca252e319d08f66bd8ccc28905
Teja mallela <40014864+tejassassin@users.noreply.github.com>
2021-06-01 20:16:04 +0530

    added _redirect'

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,9 @@ import Fade from "react-reveal/Fade";
 export default function About() {
   return (
     <div className="about">
-      <div className="abtimg"></div>
+      <a href="/about">
+        <div className="abtimg"></div>
+      </a>
       <div className="abttext">
         <div className="abttitle">ABOUT ME</div>
         <div className="abtdesc">Hi, my name is Sakshi.........</div>
