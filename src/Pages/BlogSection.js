d5e6ec4e6a65a302c56b3737badbda7258c2b8f4
Teja mallela <40014864+tejassassin@users.noreply.github.com>
2022-01-10 22:36:19 +0530

    some css changes adsavnbsea

--- src/Pages/BlogSection.js
+++ src/Pages/BlogSection.js
@@ -131,7 +131,7 @@ function BlogSection() {
                     navigation={true}
                     effect={"coverflow"}
                     centeredSlides={true}
-                    slidesPerView={window.innerWidth < 769 ? (window.innerWidth < 361  ? 1:3) : 4}
+                    slidesPerView={window.innerWidth < 769 ? (window.innerWidth < 500  ? 1:3) : 4}
                     loop={true}
                     coverflowEffect={{
                       rotate: 50,
